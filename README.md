aad51a9a9bc3e5600a183069ed2d6a9d4d5f4f89
Hiroyuki KOBAYASHI <hkob@metro-cit.ac.jp>
2021-07-13 14:14:20 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # notion-tools-alfred-workflow
 An Alfred workflow to add / change tasks to Notion.so
+
+This workflow uses [OneUpdater](https://github.com/vitorgalvao/alfred-workflows/tree/master/OneUpdater) for automatic version updates.
+
+# Keywords
+
+## an: Add Notion task for a project
+
+Simply type your task name, date and time into Alfred to create a task page to Notion.
+The date and time are optional.
+
+Title only|With date|With date and time|
+:-:|:-:|:-:
+|![no date](an1.png)|![with date](an2.png)|![with date and time](an3.png)|
+
+After typing, select a corresponding project from project lists.
+The project list was cached into this workflow.
+If you want to update the project list, type `shift` + `return` when typing a task name.
+|Project selection|Force reload project (shift + return)|
+:-:|:-:
+|![select project](selectProjects.png)|![Force reload](an4.png)|
+
+## ac: Add calendar event for Notion calendar
+
+Simply type your task name into Alfred to create a task event to Notion calendar.
+
+Title only|With date|With date and start time|With date, start time and end time|
+:-:|:-:|:-:|:-:
+|![no date](ac1.png)|![with date](ac2.png)|![with date and time](ac3.png)|![with date and time](ac4.png)|
+
+## ar: Add a refrection sentence to today's refrection page
+
+Simply type your sentence into Alfred to append the sentence block to a daily refrection page.
+![ar](ar.png)
+
+## pn: Set pomodoro property to today's tasks
+
+Select an unfinished task from list, and settle a correspond pomodoro period.
+Select task|Select pomodoro|
+:-:|:-:
+|![select task](pn1.png)|![select task](pn2.png)|
+
+# Workflow Variables
+
+- `MY_NOTION_TOKEN`: Your Notion token.
+- `TASK_ID`: Database ID for your task database.
+- `PROJECT_ID`: Database ID for you project database.
+- `MY_TZ`: Your timezone.  Defaults to "+09:00".
+- `TASK_DATE`: Date property name for the task.
+- `TASK_POMO`: Pomodoro property name for the task.  Do not use Japanese character `ポモ`, because of the NFC-NFD problem.
+- `TASK_DONE`: Task finished flag property name for the task.
+- `TASK_IS_NOT_POMO`: (Optional) Not pomodoro flag property name for the task.
+- `TASK_IS_POMO`: (Optional) Pomodoro flag property name for the task.
+- `PROJECT_NAME`: Relation title to Project for the task.  Do not use Japanese character `プロジェクト`, because of the NFC-NFD problem.
+- `REFRECTION_TITLE`: Refrection page title.
+- `NOTION_CALENDAR`: Calendar name for Notion Calendar.
+
+# Adding the variables to the Alfred workflows
+
+In the Alfred worfklow in the upper right corner click the `[x]` icon and add the values from above to the corresponding value field.
+
---
+++ notion.rb
@@ -0,0 +1,97 @@
+require "net/http"
+require "uri"
+require "json"
+require "date"
+
+# Read environments
+MY_NOTION_TOKEN = ENV["MY_NOTION_TOKEN"] # Notion token
+TASK_ID = ENV["TASK_ID"] # タスクのデータベースID
+PROJECT_ID = ENV["PROJECT_ID"] # プロジェクトのデータベースID
+TASK_NAME = ENV["TASK_NAME"] # タスクの名前
+TASK_DATE = ENV["TASK_DATE"] # タスクの日付
+TASK_DONE = ENV["TASK_DONE"] # タスクの終了フラグ
+PROJECT_NAME = ENV["PROJECT_NAME"] # プロジェクトの名前
+REFLECTION_TITLE=ENV["REFLECTION_TITLE"] # 振り返りのタスクタイトル
+TASK_POMO = ENV["TASK_POMO"] # ポモドーロの属性
+TASK_IS_POMO = ENV["TASK_IS_POMO"] || "" # ポモドーロであるときに true になる属性
+TASK_IS_NOT_POMO = ENV["TASK_IS_NOT_POMO"] || "" # ポモドーロでないときに true になる属性
+MY_TZ=ENV["MY_TZ"] # タイムゾーン
+
+# post 専用のメソッド
+def post_notion(uri, payload)
+  request = Net::HTTP::Post.new(uri)
+  request.content_type = "application/json"
+  request["Authorization"] = "Bearer #{MY_NOTION_TOKEN}"
+  request["Notion-Version"] = "2021-05-13"
+  request.body = JSON.dump(payload)
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+    http.request(request)
+  end
+  code = response.code
+  unless code == "200"
+    STDERR.print "Error!\n#{response.body}\n" # エラーの時だけエラーを表示する
+  end
+  STDERR.print response.body
+  JSON.parse(response.body)["results"]
+end
+
+def create_task(title, datetime, project_id = nil)
+  relation = project_id ? [{id: "#{project_id}"}] : []
+  post_notion URI.parse("https://api.notion.com/v1/pages"), {
+    parent: {
+      database_id: TASK_ID
+    },
+    properties: {
+      TASK_NAME => {
+        title: [
+          {
+            text: {
+              content: title
+            }
+          }
+        ]
+      },
+      TASK_DATE => {
+        type: "date",
+        date: {
+          start: datetime
+        }
+      },
+      Project: {
+        type: "relation",
+        relation: relation
+      }
+    }
+  }
+end
+
+# ページの取得
+def get_notion_pages(payload, database_id = TASK_ID)
+  post_notion URI.parse("https://api.notion.com/v1/databases/#{database_id}/query"), payload
+end
+
+# patch 専用のメソッド
+def patch_notion(uri, payload)
+  request = Net::HTTP::Patch.new(uri)
+  request.content_type = "application/json"
+  request["Authorization"] = "Bearer #{MY_NOTION_TOKEN}"
+  request["Notion-Version"] = "2021-05-13"
+  request.body = JSON.dump(payload)
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+    http.request(request)
+  end
+  code = response.code
+  unless code == "200"
+    STDERR.print "Error!\n#{response.body}\n" # エラーの時だけエラーを表示する
+  end
+end
+
+# block 要素を追加するメソッド
+def append_block_children(id, payload)
+  patch_notion URI.parse("https://api.notion.com/v1/blocks/#{id}/children"), payload
+end
+
+# page プロパティを更新するメソッド
+def update_page(id, payload)
+  patch_notion URI.parse("https://api.notion.com/v1/pages/#{id}"), payload
+end
